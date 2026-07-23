6dcc2869e4c8e415c90e652fcef1cc49b504be28
founder611 <founder@ecomonks.in>
2026-07-23 16:27:44 +0530

    Fix ALLOWED_HOSTS

--- phactor/settings.py
+++ phactor/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1fak7hjc0nvfwe%p4zrotit^$_)phz0-80_eug50_4&acrx8gl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
