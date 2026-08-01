1da78296ed2a34a5c6881568f67c913a19053623
founder611 <founder@ecomonks.in>
2026-08-01 15:44:44 +0530

    updation1

--- phactor/urls.py
+++ phactor/urls.py
@@ -4,6 +4,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),  
+    path('', include('myapp.urls')),   # <-- This is the important change
 ]
     
